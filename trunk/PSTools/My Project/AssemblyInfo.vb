6804584aa7a50ee2c6e4b7a61ff4ab2aeb2680a4
jeanlouis.persat <jeanlouis.persat@6d232958-7d05-83cd-1d04-186d2947424c>
2011-05-19 15:31:33 +0000

    * tests for CS55

--- trunk/PSTools/My Project/AssemblyInfo.vb
+++ trunk/PSTools/My Project/AssemblyInfo.vb
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.292.0")> 
+<Assembly: AssemblyVersion("1.0.307.0")> 
 <Assembly: AssemblyFileVersion("1.0.111.0")> 
